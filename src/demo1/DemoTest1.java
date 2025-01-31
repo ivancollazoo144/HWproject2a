4fea5430308cd72b2586a93a9558ab8436c0e333
ivancollazoo <collazoivan1412@gmail.com>
2025-01-31 16:42:21 -0400

    a or b is bigger?

---
+++ src/demo1/DemoTest1.java
@@ -0,0 +1,16 @@
+package demo1;
+
+public class DemoTest1 {
+	public static void main(String[] args) {
+
+		int a = 7675;
+		int b = 7655;
+
+		if (a < b) {
+			System.out.println("b is bigger");
+		} else {
+			System.out.println("A is bigger");
+		}
+	}
+
+}
